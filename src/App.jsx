617c1eeb9f3c07ec9811b91ef567bbee6053a7ae
RAJA <akularaja44@gmail.com>
2025-07-09 10:26:29 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,107 @@
 import React, { useState } from 'react';
 import './App.css';
 
-const App =() => {
+const App = () => {
   const [todoo, setTodo] = useState('');
   const [todos, setTodos] = useState([]);
-  const adding = ()=>{
-    if (todoo.trim() !== ""){
+  const [isEditing, setIsEditing] = useState(false);
+  const [editText, setEditText] = useState('');
+  const [editIndex, setEditIndex] = useState(null);
+
+  const addTodo = () => {
+    if (todoo.trim() !== '') {
       setTodos([...todos, todoo]);
       setTodo('');
     }
-  }
-  const delfun= (ind) => {
-    const newTodos = todos.filter((_, index) => index !== ind);
-    setTodos(newTodos);
-  }
-  
-  const Editbtn = (ind)=>{
-    alert("you want to edit this item");
-    
+  };
 
+  const deleteTodo = (ind) => {
+    const updated = todos.filter((_, i) => i !== ind);
+    setTodos(updated);
+    if (isEditing && editIndex === ind) {
+      setIsEditing(false);
+      setEditText('');
+      setEditIndex(null);
+    }
+  };
 
-  }
-  
+  const startEdit = (ind) => {
+    setIsEditing(true);
+    setEditText(todos[ind]);
+    setEditIndex(ind);
+  };
+
+  const applyEdit = () => {
+    if (editText.trim() !== '') {
+      const updatedTodos = todos.map((item, i) =>
+        i === editIndex ? editText : item
+      );
+      setTodos(updatedTodos);
+      setIsEditing(false);
+      setEditText('');
+      setEditIndex(null);
+    }
+  };
+
   return (
-    <>
-      <div>
-        <label htmlFor="inpText">Name : 
-        <input type="text" className="inpText" name="Name" value={todoo} onChange={(e)=>setTodo(e.target.value)}  /></label>
-        <input type="submit" className="btn" value="add" onClick={adding} />
-      </div>
-      <div>
-        <h2 className="sty">Todo list</h2>
-        <ul>
-        {todos && todos.map((todook,ind)=>(
-          <li key={ind}>{todook} <button onClick={()=> delfun(ind)}>Delete </button> </li>
-          
+    <div className="app-container">
+      <h2 className="title">📝 Todo App</h2>
 
-        ))
-}
-         </ul>
+      {/* Add Section */}
+      <div className="input-group">
+        <label>
+          Add Todo:
+          <input
+            type="text"
+            className="input-field"
+            value={todoo}
+            onChange={(e) => setTodo(e.target.value)}
+            placeholder="Enter a task..."
+          />
+        </label>
+        <button className="btn add-btn" onClick={addTodo}>
+          Add
+        </button>
       </div>
-    </>
-  )
-}
-export default App
+
+      {/* Edit Section */}
+      {isEditing && (
+        <div className="input-group">
+          <label>
+            Edit Todo:
+            <input
+              type="text"
+              className="input-field"
+              value={editText}
+              onChange={(e) => setEditText(e.target.value)}
+              placeholder="Edit selected task..."
+            />
+          </label>
+          <button className="btn update-btn" onClick={applyEdit}>
+            Update
+          </button>
+        </div>
+      )}
+
+      {/* Todo List */}
+      <ul className="todo-list">
+        <h3>Todo List</h3>
+        {todos.map((item, ind) => (
+          <li key={ind} className="todo-item">
+            <span>{item}</span>
+            <div className="btn-group">
+              <button className="btn edit-btn" onClick={() => startEdit(ind)}>
+                Edit
+              </button>
+              <button className="btn delete-btn" onClick={() => deleteTodo(ind)}>
+                Delete
+              </button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
